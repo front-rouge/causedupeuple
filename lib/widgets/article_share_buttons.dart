f021489ccc53f7ea11cf00f5b54b0ff037b50c78
d0p1 <contact@d0p1.eu>
2019-12-13 03:56:23 +0100

    add share button action

--- lib/widgets/article_share_buttons.dart
+++ lib/widgets/article_share_buttons.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share/share.dart';
 
 class ArticleShareButtons extends StatelessWidget {
   
@@ -16,14 +18,21 @@ class ArticleShareButtons extends StatelessWidget {
           color: Colors.grey,
           tooltip: "Copier le lien de l'article",
           onPressed: () {
-            /* TODO */
+            final snackBar = SnackBar(
+              content: Text("Copié ! :)"),
+              duration: Duration(microseconds: 500),
+            );
+            Clipboard.setData(ClipboardData(text: url));
+            Scaffold.of(context).showSnackBar(snackBar);
           },
         ),
         IconButton(
           icon: Icon(Icons.share),
           color: Colors.grey,
           tooltip: "Partager l'article",
-          onPressed: () { /* TODO */ },
+          onPressed: () {
+            Share.share("$title $url", subject: "Cause Du Peuple: $title");
+          },
         )
       ],
     );
